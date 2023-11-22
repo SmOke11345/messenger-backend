81e7d0ed3ea7ee05d66331528771d95bf94f040d
Artyom <Artyom.lymar@bk.ru>
2023-11-22 21:29:05 +0600

    Add simple guard for routing.

--- apps/messenger-backend/src/app/auth/strategy/jwt.strategy.ts
+++ apps/messenger-backend/src/app/auth/strategy/jwt.strategy.ts
@@ -6,16 +6,16 @@ import { ExtractJwt } from "passport-jwt";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    super({
-      // Получаем токен из заголовка
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
-      secretOrKey: jwtConstants.secret,
-    });
-  }
+    constructor() {
+        super({
+            // Получаем токен из заголовка
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            ignoreExpiration: false,
+            secretOrKey: jwtConstants.secret,
+        });
+    }
 
-  async validate(payload: any) {
-    return { user_id: payload.sub, email: payload.email };
-  }
+    async validate(payload: any) {
+        return { user_id: payload.sub, email: payload.email };
+    }
 }
--- apps/messenger-backend/src/main.ts
+++ apps/messenger-backend/src/main.ts
@@ -11,34 +11,35 @@ import passport from "passport";
 import session from "express-session";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  const globalPrefix = "api";
+    const app = await NestFactory.create(AppModule, { cors: true });
+    const globalPrefix = "api";
 
-  app.setGlobalPrefix(globalPrefix);
+    app.setGlobalPrefix(globalPrefix);
 
-  // TODO: в дальнейшем если нужно будет, реализовать раздельные ошибки в register
-  // гарантирует, что все конечные точки защищены от получения не верных данных
-  // app.useGlobalPipes(new ValidationPipe());
-  app.use(
-    session({
-      name: "NESTJS_SESSION_ID",
-      secret: "m*f~w^cMMK2CQ3:",
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-      },
-    }),
-  );
+    // TODO: в дальнейшем если нужно будет, реализовать раздельные ошибки в register
+    // гарантирует, что все конечные точки защищены от получения не верных данных
+    // app.useGlobalPipes(new ValidationPipe());
 
-  app.use(passport.initialize());
-  app.use(passport.session());
+    app.use(
+        session({
+            name: "NESTJS_SESSION_ID",
+            secret: "m*f~w^cMMK2CQ3:",
+            resave: false,
+            saveUninitialized: false,
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+            },
+        }),
+    );
 
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+    Logger.log(
+        `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    );
 }
 
 bootstrap();

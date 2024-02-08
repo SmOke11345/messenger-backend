1a20f9e85eef04bac63f46b528d2d31e8bb13634
Artyom <Artyom.lymar@bk.ru>
2024-02-08 18:45:43 +0600

    Connecting to rooms and messaging.

--- apps/messenger-backend/src/app/gateway/gateway.ts
+++ apps/messenger-backend/src/app/gateway/gateway.ts
@@ -3,18 +3,21 @@ import {
     MessageBody,
     OnGatewayConnection,
     OnGatewayDisconnect,
+    OnGatewayInit,
     SubscribeMessage,
     WebSocketGateway,
     WebSocketServer,
 } from "@nestjs/websockets";
 
 import { Server, Socket } from "socket.io";
+import { Logger } from "@nestjs/common";
 
 /*
    Сообщение на которое мы подписываемся newMessage.
    Отправляем сообщение, тот пользователь, который подключился к сокету
    и подписался на событие onMessage,
    получит его в виде строки "отправленное сообщение в newMessage".
+   emit - отправка сообщения, on - подписка.
 */
 
 @WebSocketGateway({
@@ -23,17 +26,25 @@ import { Server, Socket } from "socket.io";
         origin: ["http://localhost:4200"],
     },
 })
-export class Gateways implements OnGatewayConnection, OnGatewayDisconnect {
+export class Gateways
+    implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
     // Прямой доступ к экземпляру сервера.
     @WebSocketServer()
     server: Server;
+
+    private logger: Logger = new Logger("Gateways");
+
+    afterInit(server: any) {
+        this.logger.log("Init");
+    }
 
     /**
      * Сообщение в консоль при подключении к сокету.
      * @param client
      */
     handleConnection(client: Socket) {
-        console.log(`Client connected: ${client.id}`);
+        this.logger.log(`Client connected: ${client.id}`);
     }
 
     /**
@@ -41,20 +52,34 @@ export class Gateways implements OnGatewayConnection, OnGatewayDisconnect {
      * @param client
      */
     handleDisconnect(client: Socket) {
-        console.log(`Client disconnected: ${client.id}`);
+        this.logger.log(`Client disconnected: ${client.id}`);
     }
 
     /**
      * Отправка сообщений.
      * @param payload
+     * @param client
      */
     @SubscribeMessage("newMessage")
-    onNewMessage(@MessageBody() payload: { content: string; chatId: string }) {
+    onNewMessage(
+        @MessageBody() payload: { content: string; chatId: string },
+        @ConnectedSocket() client: Socket,
+    ) {
+        console.log(payload.content, payload.chatId);
         // Если пользователи находятся в одном чате (комнате), то прослушиваем onMessage и получаем отправленное сообщение.
         this.server.to(payload.chatId).emit("onMessage", payload.content);
     }
 
     // TODO: Добавить удаление сообщений.
+
+    // @SubscribeMessage("deleteMessage")
+    // onDeleteMessage(
+    //     @MessageBody() payload: { messageId: number; chatId: string },
+    // ) {
+    //     this.server
+    //         .to(payload.chatId)
+    //         .emit("onDeleteMessage", payload.messageId);
+    // }
 
     /**
      * Подключение к чату (комнате).
@@ -66,7 +91,37 @@ export class Gateways implements OnGatewayConnection, OnGatewayDisconnect {
         @MessageBody() chatId: string,
         @ConnectedSocket() client: Socket,
     ) {
-        client.join(chatId);
+        client.join(chatId); // Подключаемся к комнате.
+        // Для получения подключенных пользователей, в frontend.
+        this.server
+            .to(chatId)
+            .emit(
+                "onJoinChat",
+                `Пользователь ${client.id} подключился к комнате ${chatId}`,
+            );
+        this.logger.log(
+            `Пользователь ${client.id} подключился к комнате ${chatId}`,
+        );
+    }
+
+    /**
+     * Выход из чата (комнаты).
+     * @param chatId
+     * @param client
+     */
+    @SubscribeMessage("leaveChat")
+    onLeaveChat(
+        @MessageBody() chatId: string,
+        @ConnectedSocket() client: Socket,
+    ) {
+        client.leave(chatId); // Покидаем комнату.
+        // Для получения пользователей которые вышли, в frontend.
+        this.server
+            .to(chatId)
+            .emit(
+                "onJoinChat",
+                `Пользователь ${client.id} покинул комнату ${chatId}`,
+            );
     }
 
     //  TODO: Добавить выход из чата (комнаты).
